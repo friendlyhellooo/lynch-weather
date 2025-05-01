222ded11af14e3ea12470d362138a745761a03ea
Val Nuñez <val@friendlyhello.com>
2025-04-30 19:41:03 -0700

    Flexboxing
    Matching layout to figma layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,32 @@ function App() {
   const [count, setCount] = createSignal(0);
 
   return (
-    <div
-      class="min-h-screen bg-cover bg-center text-white font-sans p-6"
-      style={{ "background-image": "url('david-lynch-bg.png')" }}
-    >
-      {/* TOP ROW */}
-      <div class="flex justify-end mb-4">
-        <div class="flex flex-col gap-2 items-end">
-          <div class="bg-white/20 p-2 rounded">Los Angeles, CA</div>
-          <div class="bg-white/20 p-2 rounded">Thursday, May 1st 2025</div>
+    <div class="min-h-screen flex items-center justify-center">
+      <div
+        class="w-[400px] h-[300px] flex flex-col justify-between bg-cover bg-center text-white font-sans p-4 rounded shadow-lg"
+        style={{ "background-image": "url('/david-lynch-bg.png')" }}
+      >
+        {/* 🔝 Top Section */}
+        <div>
+          {/* Top Row */}
+          <div class="flex justify-end mb-2">
+            <div class="flex flex-col items-end gap-1">
+              <div class="bg-white/20 p-2 rounded">Los Angeles, CA</div>
+              <div class="bg-white/20 p-2 rounded">Thursday, May 1st 2025</div>
+            </div>
+          </div>
+
+          {/* Middle Row */}
+          <div class="flex justify-end gap-2">
+            <div class="bg-white/20 p-2 rounded">Image 1</div>
+            <div class="bg-white/20 p-2 rounded">Image 2</div>
+          </div>
         </div>
-      </div>
 
-      {/* MIDDLE ROW */}
-      <div class="flex justify-end gap-4 mb-4">
-        <div class="bg-white/20 p-2 rounded">Image 1</div>
-        <div class="bg-white/20 p-2 rounded">Image 1</div>
-      </div>
-
-      {/* BOTTOM ROW */}
-      <div class="flex justify-between items-end">
-        <div class="bg-white/20 p-2 rounded">Image 1</div>
-        <div class="bg-white/20 p-2 rounded">
-          Lorem ipsum sans si dolor, abracadrabra abracadabra
+        {/* 🔚 Bottom Row */}
+        <div class="flex justify-between">
+          <div class="bg-white/20 p-2 rounded">Bottom Left</div>
+          <div class="bg-white/20 p-2 rounded">Bottom Right</div>
         </div>
       </div>
     </div>
